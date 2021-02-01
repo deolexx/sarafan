50624fee33550927360bda4ea33cec619e8c3f55
Deo <lukyanenko88@gmail.com>
2021-02-01 16:26:40 +0200

    Initial commit

---
+++ src/main/java/com/deo/sarafan/controller/MessageController.java
@@ -0,0 +1,17 @@
+package com.deo.sarafan.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("message")
+public class MessageController {
+    @GetMapping
+    public String list(){
+    return "index";
+    }
+
+
+
+}
