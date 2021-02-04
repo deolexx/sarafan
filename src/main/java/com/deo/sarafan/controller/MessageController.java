90912a84bf243a44c75a0f5343680c9664f2717f
Deo <lukyanenko88@gmail.com>
2021-02-04 20:20:33 +0200

    first demo version

--- src/main/java/com/deo/sarafan/controller/MessageController.java
+++ src/main/java/com/deo/sarafan/controller/MessageController.java
@@ -4,14 +4,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 @RequestMapping("message")
 public class MessageController {
-    @GetMapping
-    public String list(){
-    return "index";
-    }
+    public List<Map<String, String>> messages = new ArrayList<Map<String, String>>() {
+        {
+            add(new HashMap<String, String>() {{
+                put("id", "1");
+                put("text", "First message");
+            }});
+            add(new HashMap<String, String>() {{
+                put("id", "1");
+                put("text", "Second message");
+            }});
+            add(new HashMap<String, String>() {{
+                put("id", "1");
+                put("text", "Third message");
+            }});
+        }
+    };
 
+    @GetMapping
+    public List<Map<String, String>> list() {
+        return messages;
+    }
 
 
 }
