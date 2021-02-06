7034ad132b024e3c5602d6e58daea6b3f69bfba3
Deo <lukyanenko88@gmail.com>
2021-02-06 09:57:44 +0200

    create all basic REST methods and test them

--- src/main/java/com/deo/sarafan/controller/MessageController.java
+++ src/main/java/com/deo/sarafan/controller/MessageController.java
@@ -20,15 +20,19 @@ public class MessageController {
                 put("text", "First message");
             }});
             add(new HashMap<String, String>() {{
-                put("id", "1");
+                put("id", "2");
                 put("text", "Second message");
             }});
             add(new HashMap<String, String>() {{
-                put("id", "1");
+                put("id", "3");
                 put("text", "Third message");
             }});
         }
     };
+
+    private Map<String, String> getMessage(String id) {
+        return messages.stream().filter(message -> message.get("id").equals(id)).findFirst().orElseThrow(NotFoundException::new);
+    }
 
     @GetMapping
     public List<Map<String, String>> list() {
@@ -42,9 +46,7 @@ public class MessageController {
         return getMessage(id);
     }
 
-    private Map<String, String> getMessage(String id) {
-        return messages.stream().filter(message -> message.get("id").equals(id)).findFirst().orElseThrow(NotFoundException::new);
-    }
+
 
 
     @PostMapping
@@ -56,10 +58,19 @@ public class MessageController {
 
     @PutMapping("{id}")
     public Map<String , String > update(@PathVariable String id,@RequestBody Map<String, String> message){
-        Map<String, String> messageFromDb = getMessage(message.get("id"));
+        Map<String, String> messageFromDb = getMessage(id);
         messageFromDb.putAll(message);
         messageFromDb.put("id",id);
         return messageFromDb;
+    }
+
+
+    @DeleteMapping("{id}")
+    public  void delete(@PathVariable String id){
+        Map<String , String > message = getMessage(id);
+        messages.remove(message);
+
+
     }
 
 }
