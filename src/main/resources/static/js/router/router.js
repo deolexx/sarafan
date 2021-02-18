3418dcc37cb518c9373b109b537e13a69c2f2f09
Deo <lukyanenko88@gmail.com>
2021-02-18 10:34:06 +0200

    still working

--- src/main/resources/static/js/router/router.js
+++ src/main/resources/static/js/router/router.js
@@ -1,9 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import MessagesList from 'components/messages/MessageList.vue'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+    {path:'/' ,component: MessagesList},
+    {path: '*',component: MessagesList}
+]
 
 export  default new VueRouter({
     routes
